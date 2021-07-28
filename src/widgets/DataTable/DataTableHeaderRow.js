79f8484a949e4468c005aee1ef6442697c0bcad5
Joshua Griffin <griffinjoshua5@gmail.com>
2021-07-28 14:25:48 +1200

    Add a prop to DataTableHeaderRow to disable *all* sorting

--- src/widgets/DataTable/DataTableHeaderRow.js
+++ src/widgets/DataTable/DataTableHeaderRow.js
@@ -22,7 +22,7 @@ import { dataTableStyles } from '../../globalStyles';
  *
  *
  */
-const DataTableHeaderRow = React.memo(({ columns, sortKey, isAscending, onPress }) => {
+const DataTableHeaderRow = React.memo(({ columns, sortKey, isAscending, onPress, isSortable }) => {
   const { headerRow, headerCells, cellText } = dataTableStyles;
   const renderCells = React.useCallback(
     () =>
@@ -41,7 +41,7 @@ const DataTableHeaderRow = React.memo(({ columns, sortKey, isAscending, onPress 
             columnKey={key}
             onPress={sortable ? onPress : null}
             sortDirection={directionForThisColumn}
-            sortable={sortable}
+            sortable={isSortable ? sortable : false}
             width={width}
             containerStyle={headerCells[alignText || 'left']}
             textStyle={cellText[alignText || 'left']}
@@ -59,12 +59,14 @@ DataTableHeaderRow.propTypes = {
   sortKey: PropTypes.string,
   isAscending: PropTypes.bool.isRequired,
   onPress: PropTypes.func,
+  isSortable: PropTypes.bool,
 };
 
 DataTableHeaderRow.defaultProps = {
   columns: [],
   sortKey: '',
   onPress: null,
+  isSortable: true,
 };
 
 export default DataTableHeaderRow;
