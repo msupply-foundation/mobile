37e302ae8df682069ac439476ca822edf92f5340
Kat <katherine@sussol.net>
2021-12-21 10:58:21 +1300

    Remove import of msupply-ble-service via yalc

--- .yalc/msupply-ble-service/lib/module/Bluetooth/types.js
+++
@@ -1,3 +0,0 @@
-import { ScanMode, Subscription, Characteristic, BleError, Device, ScanOptions, LogLevel } from 'react-native-ble-plx';
-export { ScanMode, Subscription, Characteristic, BleError, Device, ScanOptions, LogLevel };
-//# sourceMappingURL=types.js.map
--- .yalc/msupply-ble-service/lib/module/constants/index.js
+++
@@ -1,3 +0,0 @@
-export { BT510 } from './Bluetooth';
-export { BLUE_MAESTRO } from './Bluetooth';
-//# sourceMappingURL=index.js.map
